220729e0ca4bfcdbd9dd17a4e37a9266798b0327
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-24 14:31:32 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', options: { module: true } },
+          { loader: 'css-loader', options: { modules: true } },
           { loader: 'sass-loader' }
         ],
         exclude: /node_modules/
